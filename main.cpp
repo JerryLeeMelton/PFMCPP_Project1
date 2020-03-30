30d75b4d682b54cd233cb385ad4c0e9258f23c79
Jerry Lee Melton <jerryleemelton@gmail.com>
2020-03-30 22:56:34 +0000

    Completed assignment.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ Purpose:  This project will get you thinking correctly about choosing good names
  1) write out 10 nouns in the space provided below.
     Choose nouns that do things.  
     avoid choosing nouns that have things done TO them.
-    e
+    
  
  2) for each of the 10 nouns, write out 3 actions it might perform, in plain english.
     a) AVOID state-checking actions, like "Car has a horn".  i.e.
@@ -50,93 +50,102 @@ send me a DM to check your pull request
 //  example)
 //  Noun:       arm                     // 1)
 //  action 1:   the arm extends         // 2)
-arm.extend();                           // 3)
+//arm.extend();                           // 3)
 //  action 2:   the arm flexes          // 2)
-arm.flex();                             // 3)
+//arm.flex();                             // 3)
 //  action 3:   the arm rotates conter-clockwise
-arm.rotateCounterClockwise();           // 4) demonstrates CamelCase
+//arm.rotateCounterClockwise();           // 4) demonstrates CamelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
+//  Noun: Smartphone
+//  action 1: Smartphone connects to network
+//            smartphone.connectToNetwork();
+//  action 2: Smartphone checks for updates
+//            smartphone.checkForUpdate();
+//  action 3: Smartphone sends user push notification
+//            smartphone.sendPushNotification();
 
-//  action 2:
-
-//  action 3:
- 
 //  2)
-//  Noun:
-//  action 1:
+//  Noun: Dog
+//  action 1: Dog barks
+//            dog.bark();
+//  action 2: Dog sniffs
+//            dog.sniff();
+//  action 3: Dog licks
+//            dog.lick();
 
-//  action 2:
-
-//  action 3:
- 
 //  3)
-//  Noun:
-//  action 1:
+//  Noun: Cat
+//  action 1: Cat meows
+//            cat.meow();
+//  action 2: Cat scratches
+//            cat.scratch();
+//  action 3: Cat hisses
+//            cat.hiss();
 
-//  action 2:
-
-//  action 3:
- 
 //  4)
-//  Noun:
-//  action 1:
+//  Noun: Space heater 
+//  action 1: Space heater heats room
+//            spaceHeater.heatRoom();
+//  action 2: Space heater checks temperature
+//            spaceHeater.checkTemperature();
+//  action 3: Space heater pauses heating
+//            spaceHeater.pauseHeating();
 
-//  action 2:
-
-//  action 3:
- 
 //  5)
-//  Noun:
-//  action 1:
+//  Noun: Bird
+//  action 1: Bird flies
+//            bird.fly();
+//  action 2: Bird chirps
+//            bird.chirp();
+//  action 3: Bird digs for worms
+//            bird.digForWorms();
 
-//  action 2:
-
-//  action 3:
- 
 //  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: Audio interface
+//  action 1: Audio interface gets audio from input 1
+//            audioInterface.getInputOneAudio();
+//  action 2: Audio interface gets audio from computer
+//            audioInterface.getComputerAudio();
+//  action 3: Audio interface converts digital data to analog signal
+//            audioInterface.convertDigitalToAnalog();
  
 //  7)
-//  Noun:
-//  action 1:
+//  Noun: Battery Charger
+//  action 1: Battery charger charges battery
+//            batteryCharger.chargeBattery();
+//  action 2: Battery charger checks battery level
+//            batteryCharger.checkLevel();
+//  action 3: Battery charger stops charging
+//            batteryCharger.stopCharging();
 
-//  action 2:
-
-//  action 3:
- 
 //  8)
-//  Noun:
-//  action 1:
+//  Noun: Toaster Oven
+//  action 1: Toaster oven heats food
+//            toasterOven.heatFood();
+//  action 2: Toaster oven decrements timer
+//            toasterOven.decrementTimer();
+//  action 3: Toaster oven stops heating
+//            toasterOven.stopHeating();
 
-//  action 2:
-
-//  action 3:
- 
 //  9)
-//  Noun:
-//  action 1:
+//  Noun: Digital Camera
+//  action 1: Digital camera opens shutter
+//            digitalCamera.openShutter();
+//  action 2: Digital camera saves image
+//            digitalCamera.saveImage();
+//  action 3: Digital camera checks light levels
+//            digitalCamera.checkLightLevels();
 
-//  action 2:
-
-//  action 3:
- 
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: Photography Strobe
+//  action 1: Photography strobe charges next flash
+//            photographyStrobe.chargeFlash();
+//  action 2: Photography strobe fires flash
+//            photographyStrobe.fireFlash();
+//  action 3: Photography strobe blows fuse
+//            photographyStrobe.blowFuse();
 
 
 #endif
